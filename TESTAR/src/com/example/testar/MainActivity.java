9939bb7bb978cb7ad7f8a92ae99aa5a1605d48f2
markvst <conclusiondinosaurs@gmail.com>
2014-03-06 19:53:52 +0100

    ett test projekt

---
+++ TESTAR/src/com/example/testar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testar;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
